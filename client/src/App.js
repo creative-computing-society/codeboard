1453e4e359e0295796566b8fcfc42bb4d65b7b6f
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-06-09 21:11:33 +0530

    Update App.js to include routing and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Leetcode from './Components/Leetcode';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
+import './App.css'
 function App() {
   return (
-    <>
-      <h1>Hello World!!</h1>
-    </>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/leetcode" element={<Leetcode />} />
+            
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
