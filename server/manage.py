1a30dfd971609e647461bdb63bea5beba10a8050
ishan singla <singlaishan69@gmail.com>
2024-06-12 22:35:48 +0530

    updated managre.py, run all necessary commands with one

--- server/manage.py
+++ server/manage.py
@@ -2,13 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from subprocess import Popen
 
+def start_celery():
+    Popen(['celery', '-A', 'app.celery', 'worker','--pool=solo', '-l', 'info'])
+    Popen(['celery', '-A', 'app.celery', 'beat', '-l', 'info'])
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
         from django.core.management import execute_from_command_line
+        if 'runserver' in sys.argv:
+            start_celery()
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
