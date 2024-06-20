e9aa6caf8647fdd7377410b2041c39f55bc14f2b
ishan singla <singlaishan69@gmail.com>
2024-06-20 17:06:53 +0530

    login and logout, functionality

--- server/ccs_auth/urls.py
+++ server/ccs_auth/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('test_token/', views.TestToken.as_view(), name='test_token'),
     path('signup_token/', views.SignUpTokenView.as_view(), name='signup_token'),
+    path('login_token/', views.LoginTokenView.as_view(), name='login_token'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- server/ccs_auth/views.py
+++ server/ccs_auth/views.py
@@ -11,6 +11,8 @@ from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+import jwt
+'''For Testing, To be removed later'''
 class SignUPView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -31,28 +33,79 @@ class LoginView(APIView):
         token, created = Token.objects.get_or_create(user=user)
         serializer = UserSerializer(instance=user)
         return Response({'token': token.key, 'user':serializer.data}, status=status.HTTP_201_CREATED)
-    
+
+'''Code to keep the user logged in using JWT'''
 @authentication_classes([SessionAuthentication,TokenAuthentication])
 @permission_classes([IsAuthenticated])
 class TestToken(APIView):
     def get(self, request):
-        return Response({'message': 'passed for {}'.format(request.user.username)})
+        return Response({'message': 'passed for {}'.format(request.user.name)})
     
 class SignUpTokenView(APIView):
     def post(self, request):
-        # check if the user is already registered
-        if CUser.objects.filter(email=request.data['email']).exists():
-            return Response({'message': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = CUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            if CUser.objects.filter(email=request.data['email']).exists():
-                user = CUser.objects.get(email=request.data['email'])
-            else:
-                return Response({'message': 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
-            user.set_password(request.data['password'])
-            user.save()
-            token = Token.objects.create(user=user)
-            return Response({'token': token.key, 'user':serializer.data}, status=status.HTTP_201_CREATED)
-            
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        token = request.data.get('token')
+        try:
+            jwt_secret = 'secret'
+            if token is None:
+                return Response({'error': 'Token not found'}, status=status.HTTP_400_BAD_REQUEST)
+            # try:
+            #     payload = jwt.decode(token, jwt_secret, algorithms=['HS256'])
+            # except jwt.ExpiredSignatureError:
+            #     return Response({'error': 'Token expired'}, status=status.HTTP_400_BAD_REQUEST)
+            # except jwt.InvalidTokenError:
+            #     return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+            payload = request.data
+            email = payload['email']
+            name = payload['name']
+            user = get_object_or_404(CUser, email=email, name=name)
+            if user is None:
+                serializer = CUserSerializer(data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    if CUser.objects.filter(email=request.data['email']).exists():
+                        user = CUser.objects.get(email=request.data['email'])
+                    else:
+                        return Response({'message': 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
+                    user.set_password(request.data['password'])
+                    user.save()
+                    token = Token.objects.create(user=user)
+                    return Response({'token': token.key, 'user':serializer.data}, status=status.HTTP_201_CREATED)
+        except:        
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginTokenView(APIView):
+    def post(self, request):
+        token = request.data.get('token')
+        try:
+            jwt_secret = 'secret'
+            if token is None:
+                return Response({'error': 'Token not found'}, status=status.HTTP_400_BAD_REQUEST)
+            '''This code is to implement CCS authentication using JWT'''
+            # try:
+            #     payload = jwt.decode(token, jwt_secret, algorithms=['HS256'])
+            # except jwt.ExpiredSignatureError:
+            #     return Response({'error': 'Token expired'}, status=status.HTTP_400_BAD_REQUEST)
+            # except jwt.InvalidTokenError:
+            #     return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+            payload = request.data
+            email = payload['email']
+            name = payload['name']
+
+            user = get_object_or_404(CUser, email=email, name=name)
+            if user is None:
+                return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+            token, created = Token.objects.get_or_create(user=user)
+            serializer = CUserSerializer(instance=user)
+            return Response({'token': token.key, 'user':serializer.data}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            return Response({'error': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+
+@authentication_classes([SessionAuthentication,TokenAuthentication])
+@permission_classes([IsAuthenticated])
+class LogoutView(APIView):
+    def get(self, request):
+        token =Token.objects.get(user=request.user)
+        token.delete()
+        return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
