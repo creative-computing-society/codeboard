711fc34df2b00c84d46559d4941c548ccf9c7de7
ishan singla <singlaishan69@gmail.com>
2024-06-26 01:28:31 +0530

    csrf

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://api.codeboard.ccstiet.com']
 
 # Application definition
 
