ca89b151e7281c17d8c9b2199aa69a170025966c
Akarsh Srivastava <akarsh91140@gmail.com>
2024-08-14 07:51:22 +0000

    updated client

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,8 +6,8 @@ const Login = ({ onLogin }) => {
   // const clientId = process.env.REACT_APP_CLIENT_ID;
   const clientId = '667465f49c13fa9047715311'; // Replace with your actual client ID
 
-  // const callbackUrl = 'https://codeboard.ccstiet.com/authverify'; // Replace with your actual callback URL if needed
-  const callbackUrl = 'http://localhost:3000/authverify';
+  const callbackUrl = 'https://codeboard.ccstiet.com/authverify'; // Replace with your actual callback URL if needed
+  // const callbackUrl = 'http://localhost:3000/authverify';
   const handleLogin = () => {
     const authUrl = `https://auth.ccstiet.com/auth/google?clientid=${clientId}&callback=${callbackUrl}`;
     window.location.href = authUrl;
