4a12ecef8ac5533c4e78490aa81d8f108a6e1685
Ishan <singlaishan69@gmail.com>
2024-08-26 13:09:00 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 /* const SERVER_URL = "https://api.codeboard.ccstiet.com"; */
 const SERVER_URL = "https://api.knowishan.fun/";
+// const SERVER_URL = "http://127.0.0.1:4881/";
 export default SERVER_URL;
