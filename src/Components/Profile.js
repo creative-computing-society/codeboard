50a166907312efea96a48ccfcd4693c7120ce834
Ishan <singlaishan69@gmail.com>
2025-01-21 19:39:15 +0530

    back to same

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -62,7 +62,7 @@ const Profile = () => {
       }
 
       try {
-        const response = await fetch(`${BASE_URL}/questions/all/`, {
+        const response = await fetch(`${BASE_URL}/questions/today/`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
